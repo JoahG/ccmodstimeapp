11e72dba2a37fbe0f8ecd1c9e56667278ded67ab
Joah Gerstenberg <joahd@gbergfamily.com>
2013-10-01 07:26:40 -0500

    Fixed logic + added editing?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 var getCurrentTime = function(offset) {
 	var currentTime = new Date();
 	currentTime.setHours(currentTime.getHours()+offset-(currentTime.getTimezoneOffset()/60));
-    return currentTime.getHours() + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds() + " (" + (offset >= 0 ? "+" + offset.toString(): offset.toString() + ")");
+    return currentTime.getHours() + ":" + currentTime.getMinutes() + ":" + currentTime.getSeconds() + " (" + (offset >= 0 ? "+" + offset.toString(): offset.toString()) + ")";
 }
 
 var updateClocks = function() {
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,14 @@ class UsersController < ApplicationController
   	@user.save
   	redirect_to root_url
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(params[:user])
+    redirect_to root_url
+  end
 end
